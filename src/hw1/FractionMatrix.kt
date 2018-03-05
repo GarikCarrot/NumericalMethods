6660051edad6e23546cb71d086d8211942126c12
Katsz <vetier@gmail.com>
2018-03-05 19:14:18 +0300

    Fraction refactoring

--- src/hw1/FractionMatrix.kt
+++ src/hw1/FractionMatrix.kt
@@ -22,7 +22,7 @@ class FractionMatrix(private var n: Int, private var m: Int) : Matrix {
         val isFraction = matrix is FractionMatrix
         for (i in 0 until n) {
             for (j in 0 until m) {
-                values[i][j].plusAssign(if (isFraction) {
+                values[i][j] += (if (isFraction) {
                     (matrix as FractionMatrix).values[i][j]
                 } else {
                     Fraction(matrix.get(i, j))
@@ -35,7 +35,7 @@ class FractionMatrix(private var n: Int, private var m: Int) : Matrix {
         val isFraction = matrix is FractionMatrix
         for (i in 0 until n) {
             for (j in 0 until m) {
-                values[i][j].minusAssign(if (isFraction) {
+                values[i][j] -= (if (isFraction) {
                     (matrix as FractionMatrix).values[i][j]
                 } else {
                     Fraction(matrix.get(i, j))
@@ -52,7 +52,7 @@ class FractionMatrix(private var n: Int, private var m: Int) : Matrix {
         for (i in 0 until n) {
             for (j in 0 until m) {
                 for (k in 0 until nM) {
-                    nValues[i][j].plusAssign(if (isFraction) {
+                    nValues[i][j] += (if (isFraction) {
                         values[i][k] * (matrix as FractionMatrix).values[k][j]
                     } else {
                         values[i][k] * Fraction(matrix.get(k, j))
@@ -108,7 +108,7 @@ class FractionMatrix(private var n: Int, private var m: Int) : Matrix {
 
     override fun determinant(): Double = determinantFraction().toDouble()
 
-    fun determinantFraction():Fraction = subMatrix().determinant()
+    fun determinantFraction(): Fraction = subMatrix().determinant()
 
     override fun size(): Pair<Int, Int> = Pair(n, m)
 
@@ -123,103 +123,99 @@ class FractionMatrix(private var n: Int, private var m: Int) : Matrix {
         return true
     }
 
-    public class Fraction {
-        private var numerator = BigInteger.ZERO
-        private var denominator = BigInteger.ONE
+    class Fraction {
+        private val numerator: BigInteger
+        private val denominator: BigInteger
 
-        constructor()
-
-        constructor(fraction: Fraction) {
-            numerator = fraction.numerator
-            denominator = fraction.denominator
+        private fun toNormalForm(n: BigInteger, d: BigInteger): Pair<BigInteger, BigInteger> {
+            var gcd = gcd(n, d)
+            if (d < BigInteger.ZERO) gcd = -gcd
+            return n / gcd to d / gcd
         }
 
-        constructor(num: String, denom: String) {
-            numerator = BigInteger(num)
-            denominator = BigInteger(denom)
+        constructor(numerator: BigInteger, denominator: BigInteger) {
+            val normal = toNormalForm(numerator, denominator)
+            this.numerator = normal.first
+            this.denominator = normal.second
         }
+
+        constructor() : this(BigInteger.ZERO, BigInteger.ONE)
+
+        constructor(fraction: Fraction) : this(fraction.numerator, fraction.denominator)
+
+        constructor(num: String, denom: String) : this(BigInteger(num), BigInteger(denom))
 
         constructor(value: Double) {
             val s = value.toString()
-            if (s.contains('.')) {
-                val sp = s.split('.')
-                numerator = BigInteger(sp[0] + sp[1])
-                denominator = BigInteger.TEN.pow(sp[1].length)
-                flow()
-            } else {
-                numerator = BigInteger(s)
+            val nnn: BigInteger
+            val den: BigInteger
+            when {
+                s.contains('.') -> {
+                    val sp = s.split('.')
+                    nnn = BigInteger(sp[0] + sp[1])
+                    den = BigInteger.TEN.pow(sp[1].length)
+                }
+                else -> {
+                    nnn = BigInteger(s)
+                    den = BigInteger.ONE
+                }
             }
+
+            val normal = toNormalForm(nnn, den)
+            this.numerator = normal.first
+            this.denominator = normal.second
         }
 
         private fun gcd(a0: BigInteger, b0: BigInteger): BigInteger {
-            var a = a0
-            var b = b0
+            var a = a0.abs()
+            var b = b0.abs()
+            if (b > a) a = b.also { b = a }
             while (b != BigInteger.ZERO) {
                 a = b.also { b = a.mod(b) }
             }
             return a
         }
 
-        fun flow() {
-            val gcd = gcd(numerator, denominator)
-            numerator /= gcd
-            denominator /= gcd
-        }
+        operator fun plus(value: Fraction): Fraction =
+                Fraction(value.denominator * numerator + value.numerator * denominator,
+                        denominator * value.denominator)
 
-        operator fun plus(value: Fraction): Fraction = assign { it += value }
+        operator fun minus(value: Fraction): Fraction = Fraction(
+                value.denominator * numerator - value.numerator * denominator,
+                denominator * value.denominator)
 
-        operator fun minus(value: Fraction): Fraction = assign { it -= value }
+        operator fun times(value: Fraction): Fraction = Fraction(
+                numerator * value.numerator,
+                denominator * value.denominator)
 
-        operator fun times(value: Fraction): Fraction = assign { it *= value }
+        operator fun div(value: Fraction): Fraction = Fraction(
+                numerator * value.denominator,
+                denominator * value.numerator)
 
-        operator fun div(value: Fraction): Fraction = assign { it /= value }
+        operator fun unaryMinus(): Fraction = Fraction(-numerator, denominator)
 
-        operator fun unaryMinus() : Fraction {
-            val result = Fraction(this)
-            result.numerator = -result.numerator
-            return result
-        }
-
-        private fun assign(v: (Fraction) -> Unit): Fraction {
-            val result = Fraction(this)
-            v(result)
-            return result
-        }
-
-        operator fun plusAssign(value: Fraction) {
-            numerator = value.denominator * numerator + value.numerator * denominator
-            denominator *= value.denominator
-            flow()
-        }
-
-        operator fun minusAssign(value: Fraction) {
-            numerator = value.denominator * numerator - value.numerator * denominator
-            denominator *= value.denominator
-            flow()
-        }
-
-        operator fun timesAssign(value: Fraction) {
-            numerator *= value.numerator
-            denominator *= value.denominator
-            flow()
-        }
-
-        operator fun divAssign(value: Fraction) {
-            numerator *= value.denominator
-            denominator *= value.numerator
-            flow()
-        }
-
-        override fun equals(other: Any?): Boolean {
-            if (other !is Fraction) return false
-            flow()
-            other.flow()
-            return numerator == other.numerator && denominator == other.denominator
-        }
 
         fun toDouble(): Double = (numerator / denominator).toDouble()
 
         override fun toString(): String = numerator.toString() + "/" + denominator.toString()
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Fraction
+
+            if (numerator != other.numerator) return false
+            if (denominator != other.denominator) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = numerator.hashCode()
+            result = 31 * result + denominator.hashCode()
+            return result
+        }
     }
 
     private fun subMatrix(): SubMatrix = SubMatrix(this, n, n, n)
@@ -261,8 +257,9 @@ class FractionMatrix(private var n: Int, private var m: Int) : Matrix {
             var result = Fraction()
             for (i in 0 until size) {
                 val d = SubMatrix(this, i, 0, size - 1).determinant()
-                if (i % 2 == 0) result = result + get(i, 0) * d
-                else result = result - get(i, 0) * d
+                result +=
+                        if (i % 2 == 0) get(i, 0) * d
+                        else -get(i, 0) * d
             }
             return result
         }
